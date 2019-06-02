386180c7286e7ab90679084b231b69c279c2149b
Shahein Moussavi <shahein.moussavi@turner.com>
2019-06-01 23:14:38 -0400

    more stuff that didn't get added

--- scripts/helpers/functions.sh
+++ scripts/helpers/functions.sh
@@ -46,3 +46,14 @@ system_is_linux() {
   fi
 }
 
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+gt() {
+  is_in_git_repo || return
+
+  git tag --sort -version:refname |
+    fzf --multi --preview-window right:70% \
+        --preview 'git show --color=always{} | head -'$LINES
+}
